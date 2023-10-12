d9484c36b54ebe2c489959961ef1ed10784b55a9
Alessandroang <133670463+Alessandroang@users.noreply.github.com>
2023-10-12 17:17:06 +0200

    add file comics.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends ('layouts.app')
+
+@section('page-title', 'About')
+    
+
+@section('main-content')
+    <section>
+        <h1>About</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi ullam nobis assumenda quam eaque, est quaerat ipsum officia beatae ad? Voluptate facilis hic laboriosam, pariatur porro nemo debitis repellendus fuga!</p>
+    </section>
+@endsection
